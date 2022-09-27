aebdd4a67a2ac53808cbf3a0c62ca59e9f8900b6
Francisco Castro <fd.casgom@gmail.com>
2022-09-27 08:19:22 -0400

    fix comments

--- src/RandomNumber.java
+++ src/RandomNumber.java
@@ -2,35 +2,35 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-class RandomNumber {    // This class contains a method that randomizes the player order,
-                        // in order to more fairly select teams
+class RandomNumber {
 
     public List<Player> randomListGenerator(List<Player> sortedPlayers) {
-        Random rand = new Random();                         // Creates an object of the random class
-        int upperbound = sortedPlayers.size();              // Sets the maximum random number to the number of players
-        int intRandom = 0;                                  // Initializes a variable to randomize numbers
+        Random rand = new Random();
+        int upperbound = sortedPlayers.size();
+        int intRandom = 0;
 
-        int number;                                         // Initializes a variable to store random numbers
+        int selectedNumber;
         List<Integer> randomNumbers;
         randomNumbers = new ArrayList<>();
-        number = intRandom;
-        randomNumbers.add(number);
+        selectedNumber = intRandom;
+        randomNumbers.add(selectedNumber);
 
-        while (randomNumbers.size() != sortedPlayers.size()) {  // This loop will select a random number and check if
-            intRandom = rand.nextInt(upperbound);               // the corresponding player has already been selected
-            number = intRandom;
-            if (!randomNumbers.contains(number)) {
-                number = intRandom;
-                randomNumbers.add(number);
+        while (randomNumbers.size() != sortedPlayers.size()) {
+            intRandom = rand.nextInt(upperbound);
+            selectedNumber = intRandom;
+            if (!randomNumbers.contains(selectedNumber)) {
+                selectedNumber = intRandom;
+                randomNumbers.add(selectedNumber);
             }
         }
-        //System.out.println(randomNumbers);
+
         List<Player> selectedNames;
         selectedNames = new ArrayList<>();
 
 
         for (int i = 0; i < sortedPlayers.size(); i++) {
             int a = randomNumbers.get(i);
+            sortedPlayers.get(a).teams = new ArrayList<>();
             selectedNames.add(sortedPlayers.get(a));
             System.out.println(selectedNames);
         }
